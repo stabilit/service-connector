f32e4b3dab919973c3cb396ed40756bdcc5dd319
Joël Traber <joel.traber@stabilit.ch>
2009-12-28 09:22:20 +0000

    decorator pattern added

---
+++ java/pattern/src/com/stabilit/pattern/decorator/Creme.java
@@ -0,0 +1,41 @@
+/*
+ *-----------------------------------------------------------------------------*
+ *                            Copyright © 2010 by                              *
+ *                    STABILIT Informatik AG, Switzerland                      *
+ *                            ALL RIGHTS RESERVED                              *
+ *                                                                             *
+ * Valid license from STABILIT is required for possession, use or copying.     *
+ * This software or any other copies thereof may not be provided or otherwise  *
+ * made available to any other person. No title to and ownership of the        *
+ * software is hereby transferred. The information in this software is subject *
+ * to change without notice and should not be construed as a commitment by     *
+ * STABILIT Informatik AG.                                                     *
+ *                                                                             *
+ * All referenced products are trademarks of their respective owners.          *
+ *-----------------------------------------------------------------------------*
+*/
+/**
+ * 
+ */
+package com.stabilit.pattern.decorator;
+
+/**
+ * @author JTraber
+ *
+ */
+public class Creme extends CondimentDecorator {
+	
+	Beverage beverage;
+
+	public Creme(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	public String getDescription() {
+		return beverage.getDescription() + ",Creme";
+	}
+
+	public float cost() {
+		return 0.3f + beverage.cost();
+	}
+}
