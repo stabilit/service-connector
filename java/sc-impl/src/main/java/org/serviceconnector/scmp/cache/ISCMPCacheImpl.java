1d203ce8ecbeaaacda49145f558d8dfbdf322425
Jan Trnka <jan.trnka@stabilit.ch>
2010-11-09 10:50:35 +0000

    sc-lib replaces sc-impl

--- java/sc-impl/src/main/java/org/serviceconnector/scmp/cache/ISCMPCacheImpl.java
+++
@@ -1,7 +0,0 @@
-package org.serviceconnector.scmp.cache;
-
-public interface ISCMPCacheImpl {
-	public abstract Object get(Object key);
-	public abstract void put(Object key, Object value);
-	public abstract boolean remove(Object key);
-}
