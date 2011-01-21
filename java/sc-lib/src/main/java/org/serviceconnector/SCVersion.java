13cc30d4441f5dde0c46c090dc2bcb5e006ae4d1
Jan Trnka <jan.trnka@stabilit.ch>
2011-01-21 10:36:07 +0000

    open T1.1-5

--- java/sc-lib/src/main/java/org/serviceconnector/SCVersion.java
+++ java/sc-lib/src/main/java/org/serviceconnector/SCVersion.java
@@ -54,7 +54,7 @@ import org.serviceconnector.scmp.SCMPError;
 public enum SCVersion {
 
 	/** The current version. */
-	CURRENT(1, 1, 4),
+	CURRENT(1, 1, 5),
 	/** The version to make tests - DO NOT CHANGE ! */
 	TEST(3, 2, 5);
 
