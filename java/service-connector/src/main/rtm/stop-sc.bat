c435a23d9d290b1b35a1818a339a62f535a320ae
Jan Trnka <jan.trnka@stabilit.ch>
2014-02-03 23:31:00 +0000

    Agreement with SIX Stephan Keel to change the default and use -config

--- java/service-connector/src/main/rtm/stop-sc.bat
+++ java/service-connector/src/main/rtm/stop-sc.bat
@@ -5,6 +5,11 @@ rem
 rem set default directory
 set DIRNAME=%CD%
 cd "%~dp0"
-rem sc-console is used to stop SC
+rem sc-console is used to send kill command to SC
+rem either configuration file 
+rem 		 -config ../conf/sc.properties or 
+rem or host and port
+rem 		-h localhost -p 9000
+rem must eb provided to reach the SC
 java -Dlog4j.configuration=file:..\conf\log4j-console.properties -jar sc-console.jar -config ../conf/sc.properties kill
 cd %DIRNAME%
