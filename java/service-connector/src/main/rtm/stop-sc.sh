c7c9aa114b290e4d0a5b911593e4304d0023ef18
Joël Traber <joel.traber@stabilit.ch>
2015-01-28 11:26:26 +0000

    fixed sh scripts, wrong (windows) line breaks

--- java/service-connector/src/main/rtm/stop-sc.sh
+++ java/service-connector/src/main/rtm/stop-sc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #  ** ATTENTION **
 #  THIS SCRIPT MAY BE REFERENCED BY THE DEAMON SCRIPT
-rem
 # sc-console is used to send kill command to SC
 # either configuration file 
 # 		 -config ../conf/sc.properties or 
