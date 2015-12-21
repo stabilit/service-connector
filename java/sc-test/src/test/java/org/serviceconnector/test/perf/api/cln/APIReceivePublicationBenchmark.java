34e2a84e0c597085c4f5d0b286f8387216faf556
Joël Traber <joel.traber@stabilit.ch>
2015-12-21 13:25:45 +0000

    revert wrongly committed changes

--- java/sc-test/src/test/java/org/serviceconnector/test/perf/api/cln/APIReceivePublicationBenchmark.java
+++ java/sc-test/src/test/java/org/serviceconnector/test/perf/api/cln/APIReceivePublicationBenchmark.java
@@ -1,19 +1,3 @@
-/*-----------------------------------------------------------------------------*
- *                                                                             *
- *       Copyright Â© 2010 STABILIT Informatik AG, Switzerland                  *
- *                                                                             *
- *  Licensed under the Apache License, Version 2.0 (the "License");            *
- *  you may not use this file except in compliance with the License.           *
- *  You may obtain a copy of the License at                                    *
- *                                                                             *
- *  http://www.apache.org/licenses/LICENSE-2.0                                 *
- *                                                                             *
- *  Unless required by applicable law or agreed to in writing, software        *
- *  distributed under the License is distributed on an "AS IS" BASIS,          *
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
- *  See the License for the specific language governing permissions and        *
- *  limitations under the License.                                             *
- *-----------------------------------------------------------------------------*/
 package org.serviceconnector.test.perf.api.cln;
 
 import org.junit.Assert;
@@ -47,7 +31,7 @@ public class APIReceivePublicationBenchmark extends APIPerfSuperClientTest {
 		cbk.waitForMessage(120);
 		if (cbk.getMessageCount() == nrMessages) {
 			long perf = nrMessages * 1000 / cbk.getDifference();
-			testLogger.info(nrMessages + "msg ï¿½ 128 byte performance=" + perf + " msg/sec.");
+			testLogger.info(nrMessages + "msg à 128 byte performance=" + perf + " msg/sec.");
 			Assert.assertTrue("Performance not fast enough, only" + perf + " msg/sec.", perf > 1000);
 		}
 		publishService.unsubscribe();
@@ -72,7 +56,7 @@ public class APIReceivePublicationBenchmark extends APIPerfSuperClientTest {
 		cbk.waitForMessage(120);
 		if (cbk.getMessageCount() == nrMessages) {
 			long perf = nrMessages * 1000 / cbk.getDifference();
-			testLogger.info(nrMessages + "msg ï¿½ 128 byte performance" + perf + " msg/sec.");
+			testLogger.info(nrMessages + "msg à 128 byte performance" + perf + " msg/sec.");
 			Assert.assertTrue("Performance not fast enough, only" + perf + " msg/sec.", perf > 1000);
 		}
 		publishService.unsubscribe();
