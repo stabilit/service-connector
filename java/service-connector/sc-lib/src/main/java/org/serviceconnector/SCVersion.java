113d07a6927ffaa4f3186bc8e09990bba8c79f9c
Martin Zukal <martin@zukal.net>
2016-01-19 14:44:05 +0000

    set version to 2.0-1 in SCVersion

--- java/service-connector/sc-lib/src/main/java/org/serviceconnector/SCVersion.java
+++ java/service-connector/sc-lib/src/main/java/org/serviceconnector/SCVersion.java
@@ -59,7 +59,7 @@ import org.serviceconnector.scmp.SCMPError;
 public enum SCVersion {
 
 	/** The current version. */
-	CURRENT(1, 3, 33),
+	CURRENT(2, 0, 1),
 	/** The version to make tests - DO NOT CHANGE ! */
 	TEST(3, 2, 5);
 
