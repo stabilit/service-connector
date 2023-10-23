ceb46c441ba8872650ca883d39f87a1dcd195f2a
Martin Zukal <martin@zukal.net>
2023-10-23 14:23:45 +0200

    set current SC version to 4.0.0

--- java/service-connector/sc-lib/src/main/java/org/serviceconnector/SCVersion.java
+++ java/service-connector/sc-lib/src/main/java/org/serviceconnector/SCVersion.java
@@ -56,7 +56,7 @@ import org.serviceconnector.scmp.SCMPError;
 public enum SCVersion {
 
 	/** The current version. */
-	CURRENT(3, 6, 0),
+	CURRENT(4, 0, 0),
 	/** The version to make tests - DO NOT CHANGE ! */
 	TEST(3, 2, 5);
 
